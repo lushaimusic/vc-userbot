d8c183d6bdad0dc16939d2bde00793b7fea61df5
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2021-12-05 00:06:50 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv
+from pyrogram import Client, filters
+from pytgcalls import PyTgCalls
+
+# For Local Deploy
+if os.path.exists(".env"):
+    load_dotenv(".env")
+    
+# Necessary Vars
+API_ID = int(os.getenv("API_ID", ""))
+API_HASH = os.getenv("API_HASH", "")
+SESSION = os.getenv("SESSION")
+HNDLR = os.getenv("HNDLR", "!")
+GROUP_MODE = os.getenv("GROUP_MODE", "True")
+
+
+contact_filter = filters.create(
+    lambda _, __, message:
+    (message.from_user and message.from_user.is_contact) or message.outgoing
+)
+
+
+if GROUP_MODE == ("True" or "true"):
+    grp = True
+else:
+    grp = False
+
+GRPPLAY = grp
+bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="plugins"))
+call_py = PyTgCalls(bot)
